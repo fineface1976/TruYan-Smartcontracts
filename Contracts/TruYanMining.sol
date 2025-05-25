c9d5eb0bfc1f2150dbb7919d20145e5788b9e8e1
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-25 19:07:53 +0100

    Update TruYanMining.sol

--- Contracts/TruYanMining.sol
+++ Contracts/TruYanMining.sol
@@ -1,1 +1,26 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MZLx.sol";
+
+contract TruYanMining is Ownable {
+    MZLx public token;
+    uint256 public dailyReward = 1.2 * 10**18; // 1.2 MZLx/day
+    mapping(address => uint256) public lastMined;
+
+    constructor(address _token) {
+        token = MZLx(_token);
+    }
+
+    function mine() external {
+        require(block.timestamp >= lastMined[msg.sender] + 1 days, "Wait 24h");
+        lastMined[msg.sender] = block.timestamp;
+        token.mint(msg.sender, dailyReward);
+    }
+
+    function setDailyReward(uint256 _newReward) external onlyOwner {
+        dailyReward = _newReward;
+    }
+}
